ed4b737828e62422fd9cff986430fb45c65dc06f
Manvi Kumari Shah <72203675+manvikri22@users.noreply.github.com>
2022-01-26 17:34:29 +0530

    Added solution - LeetHub

--- Minimum swaps and K together - GFG/minimum-swaps-and-k-together.java
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.java
@@ -53,36 +53,31 @@ class Complete{
     // Function for finding maximum and value pair
     public static int minSwap (int arr[], int n, int k) {
         //Complete the function
-        int window_size = 0;
-        
-        for(int i =0;i<n;i++)
+        int count = 0;
+        for (int i = 0; i < n; ++i)
+        if (arr[i] <= k)
+            ++count;
+     
+        int bad = 0;
+        for (int i = 0; i < count; ++i)
+        if (arr[i] > k)
+            ++bad;
+     
+     
+        int ans = bad;
+        for (int i = 0, j = count; j < n; ++i, ++j) 
         {
-            if(arr[i]<=k)
-            {
-                window_size++;
-            }
+     
+            if (arr[i] > k)
+                --bad;
+         
+            if (arr[j] > k)
+                ++bad;
+         
+            ans = Math.min(ans, bad);
         }
-        
-        int good_elements = 0;
-       
-      for(int i = 0; i < window_size; i++) 
-      {
-        if(arr[i] <= k)
-          good_elements++;
-      }
-            
-      int max_good_elements = good_elements;
-            
-      for(int i = 0; i < n - window_size; i++) 
-      {
-         if(arr[i] <= k) 
-            good_elements--;
-         if(arr[i + window_size] <= k)
-            good_elements++;
-         max_good_elements = Math.max(max_good_elements, good_elements);
-       }
-            
-       return (window_size - max_good_elements);
+        return ans;
+    
     }
     
     
