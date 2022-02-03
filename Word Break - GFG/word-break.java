89816da3e3b1865a08fbb0fdc775198419f34c3e
Manvi Kumari Shah <72203675+manvikri22@users.noreply.github.com>
2022-02-03 22:24:17 +0530

    Added solution - LeetHub

---
+++ Word Break - GFG/word-break.java
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+public class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n;
+                    n = sc.nextInt();
+                    ArrayList<String> arr = new ArrayList<String>();
+                    for(int i = 0;i<n;i++)
+                        {
+                            String p = sc.next();
+                            arr.add(p);
+                        }
+                    String line = sc.next();
+                    Sol obj = new Sol();  
+                    System.out.println(obj.wordBreak(line,arr));  
+                    
+                }
+        }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    
+   public static int wordBreak(String A, ArrayList<String> B )
+   {
+       return sol(B,A);
+       
+   }
+   static int sol(ArrayList<String> B,String A)
+   {
+       int count=0;
+       if(A.length()==0)
+           count++;
+       for(int k=1;k<=A.length();k++)
+       {
+           int temp=0;
+           if(B.contains(A.substring(0,k)))
+             temp+=sol(B,A.substring(k));
+           count+=temp;  
+       }       
+         return count;
+   }
+}
