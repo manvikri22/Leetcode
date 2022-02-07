b23986940bddfe89f4e7969a47baaac1f8c9dc0a
Manvi Kumari Shah <72203675+manvikri22@users.noreply.github.com>
2022-02-07 19:03:33 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.java
+++ Edit Distance - GFG/edit-distance.java
@@ -23,41 +23,43 @@ class GFG {
 
 class Solution {
     public int editDistance(String s, String t) {
-     String s1 = s;
-        String s2 = t;
-        int m = s1.length();
-        int n = s2.length();
+        // Code here
+        int m  = s.length();
+        int n = t.length();
         
         int dp[][] = new int[m+1][n+1];
         
-        return solution(s1 , s2 , m , n , dp);
-    }
-    
-    static int solution(String s1 , String s2 , int m , int n , int dp[][]){
-        
-        for(int i=0 ; i<=m ; i++){
-            for(int j=0 ; j<=n ; j++){
-                
+        for(int i =0;i<=m;i++)
+        {
+            for(int j =0;j<=n;j++)
+            {
                 if(i==0)
-                    dp[i][j] = j;
-                
-                if(j==0)
+                {
+                    dp[i][j]=j;
+                }
+                else if(j==0)
+                {
                     dp[i][j] = i;
-            }
-        }
-        dp[0][0] = 0;
-        
-        for(int i=1 ; i<=m ; i++){
-            for(int j=1 ; j<=n ; j++){
-                
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                }
+                else if(s.charAt(i-1) == t.charAt(j-1))
+                {
                     dp[i][j] = dp[i-1][j-1];
                 }
-                else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j-1] , Math.min(dp[i][j-1] , dp[i-1][j]));
+                else
+                {
+                    dp[i][j]= 1+ min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1]);
                 }
             }
         }
         return dp[m][n];
+    }
+    static int min(int x, int y, int z)
+    {
+        if(x<=y && x<=z)
+        return x;
+        if(y<=x && y<=z)
+        return y;
+        else
+        return z;
     }
 }
