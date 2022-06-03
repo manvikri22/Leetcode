d4a63cddb5116fda4139c87ea7e2f797b104e6f0
Manvi Kumari Shah <72203675+manvikri22@users.noreply.github.com>
2022-06-03 20:10:44 +0530

    Time: 3 ms (37.72%), Space: 45 MB (22.73%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,28 +1,32 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
-        List<Integer> ds = new ArrayList<>();
-        printper(nums, ans, ds, freq);
+        
+        printper(0,nums, ans);
         return ans;
     }
-    public void printper(int[] nums,List<List<Integer>> ans,List<Integer> ds, boolean[] freq )
+    public void printper(int index,int[] nums,List<List<Integer>> ans)
     {
-        if(ds.size()==nums.length)
+        if(index==nums.length)
         {
+            List<Integer> ds = new ArrayList<>();
+            for(int i =0;i<nums.length;i++)
+            {
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList(ds));
             return ;
         }
-        for(int i =0;i<nums.length;i++)
+        for(int i =index;i<nums.length;i++)
         {
-            if(freq[i]==false)
-            {
-                freq[i]= true;
-                ds.add(nums[i]);
-                printper(nums,ans, ds, freq);
-                ds.remove(ds.size()-1);
-                freq[i]= false;
-            }
+            swap(i,index,nums);
+            printper(index+1,nums,ans);
+            swap(i,index,nums);
         }
+    }
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
